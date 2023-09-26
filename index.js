575848ab86f59d4b937f45f3face8bc222e48013
mbari60 <kevinmbari600@gmail.com>
2023-09-26 12:24:55 +0300

    objects-assignment -done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+     name:"kevin",
+     address:"Nairobi"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+     const updatedEmployee = {...employee};
+     updatedEmployee[key] = value;
+
+     return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee
+}
+
+ function deleteFromEmployeeByKey(employee, key){
+      const deletedEmployee = { ...employee };
+      delete deletedEmployee[key];
+
+      return deletedEmployee;
+ }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+
+  return employee;
+}
+
+
+
